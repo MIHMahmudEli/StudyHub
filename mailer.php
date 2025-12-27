59aa1ea3e7e2ddc43fc5fae3c124eb81341dcfd3
MIHMahmudEli <mdmohsin11222@gmail.com>
2025-12-27 13:53:45 +0600

    new bug fix

--- mailer.php
+++ mailer.php
@@ -15,7 +15,7 @@ function sendOTP($email, $name, $otp, $type) {
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'studyhubteam.official@gmail.com';
-        $mail->Password   = 'flalqppbuuqrrkxddsfasfaa'; // Use app password for Gmail
+        $mail->Password   = 'dfjadsflafalsdflasdjf'; // Use app password for Gmail
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
